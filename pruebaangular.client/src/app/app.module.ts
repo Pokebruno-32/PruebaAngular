fd6e23705ed5e1e4889ffa93a537b52fc0fc5ea1
Pokebruno-32 <brunomoal27@hotmail.com>
2025-09-03 13:17:02 -0600

    Agregar archivos de proyecto.

---
+++ pruebaangular.client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule, HttpClientModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
